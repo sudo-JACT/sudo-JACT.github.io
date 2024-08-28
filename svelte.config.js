b5131745fe5413c7b123f2259df1629418de07e4
1MD0NK3Y <jacopino.toma@gmail.com>
2024-08-28 19:57:56 +0200

    static

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 			'@utils': './src/lib/utils'
 		},
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
+			base: process.env.NODE_ENV === 'production' ? base : '/sudo-JACT.github.io'
 		}
 	}
 };
