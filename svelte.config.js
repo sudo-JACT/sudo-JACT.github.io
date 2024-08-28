8aecdf55457710ad4be25b18b9377a115afea2b7
1MD0NK3Y <jacopino.toma@gmail.com>
2024-08-28 19:10:38 +0200

    idk

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = 'https://sudo-jact.github.io/';
+const base = '/public';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
