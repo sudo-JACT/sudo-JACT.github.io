403fd5f90e0a24141c9e46beb9ead837860d6aa7
sudo-JACT <sudo-JACT@users.noreply.github.com>
2024-08-30 09:28:14 +0000

    deploy: 976c6faeda33f9f030fd0b46eadf425f5bc61c9a

--- _app/immutable/chunks/skills.9e3fe5b3.js
+++ _app/immutable/chunks/skills.9e3fe5b3.js
@@ -1,4 +1,4 @@
-import{A as n}from"./UIcon.3f7a2e59.js";const m=`# Svelte
+import{A as n}from"./UIcon.effe5176.js";const m=`# Svelte
 
 ---
 
--- _app/immutable/chunks/stores.7282dd14.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.f41d2648.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
