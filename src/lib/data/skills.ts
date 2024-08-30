d876bc2d6e9d92146efe2dd29ec80c2474142138
1MD0NK3Y <jacopino.toma@gmail.com>
2024-08-30 11:52:56 +0200

    blue instend of cyan

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -73,7 +73,7 @@ export const items = [
 	}),
 	defineSkill({
 		slug: 'python',
-		color: 'cyan',
+		color: 'blue',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
 		logo: Assets.Python,
